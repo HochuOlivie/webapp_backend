bcb70a9b86468e1727fe7c7dbf0219aa1995eb72
HochuOlivie <49933393+HochuOlivie@users.noreply.github.com>
2022-09-28 23:20:02 +0500

    buttons

--- telegram/main/branches.py
+++ telegram/main/branches.py
@@ -24,14 +24,26 @@ class Main:
         # self.dp.register_message_handler(self._name_handler, state=States.name)
         self.dp.register_callback_query_handler(self._delete_order, text_startswith='order_delete', state="*")
         self.dp.register_callback_query_handler(self._delete_offer, text_startswith='offer_delete', state="*")
+        self.dp.register_callback_query_handler(self._accept_order, text_startswith='order_accept', state="*")
+        self.dp.register_callback_query_handler(self._decline_order, text_startswith='order_decline', state="*")
 
     async def _delete_order(self, callback: types.CallbackQuery):
         _, order_id = callback.data.split(':')
         await sync_to_async((await Order.objects.aget(id=order_id)).delete)()
-        logging.debug('1234123412341234123412341234123412341233412341234')
         await callback.message.edit_text('Курьеры больше не будут видеть ваш заказ')
 
     async def _delete_offer(self, callback: types.CallbackQuery):
         _, offer_id = callback.data.split(':')
         await sync_to_async((await Offer.objects.aget(id=offer_id)).delete)()
         await callback.message.edit_text('Заказчики больше не будут видеть ваше предложение')
+
+    async def _accept_order(self, callback: types.CallbackQuery):
+        _, order_id = callback.data.split(':')
+        order = await Order.objects.aget(id=order_id)
+        await callback.message.edit_text(f'Контакт: @{order.user.tg_username}')
+        await self.bot.send_message(order.user.tg_id, f'Партнер согласился на доставку\n\nКонтакт: @{callback.from_user.username}')
+
+    async def _decline_order(self, callback: types.CallbackQuery):
+        _, order_id = callback.data.split(':')
+        order = await Order.objects.aget(id=order_id)
+        await callback.message.edit_text(f'Вы отказались от заказа')
